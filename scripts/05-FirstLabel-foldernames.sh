d4258190013611883b8e75fe2226205af6678e10
Vanessa Pitchford Cristobal <rc.vanessa87@gmail.com>
2018-04-10 14:31:15 +0100

    Set up for latest script

---
+++ scripts/05-FirstLabel-foldernames.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Usage:       It will convert any markdown into a pdf. The markdown must be in a folder,
+#              The markdown can be in a folder with any number of subfolders in it.
+#
+# Description: Sends files to Pandoc for conversion with the intention of getting a final pdf as a result.
+#
+# Packages: You will need Parallel, and Pandoc.
+#
+
